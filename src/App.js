e2c2a7020ebfb06ae2e64b8714b356a24d809e51
avikodre03 <akodre111@gmail.com>
2023-03-16 11:39:52 +0530

    Quiz done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { questions } from './Components/Questions'
 
 function App() {
+  // const Right = { background: "green" }
+  // const wrong = { background: "red" }
+  const [currentvalue, setCurrentvalue] = useState(0)
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0)
+  // const [ansbg, setansbg] = useState("")
+  const [clicked, setclicked] = useState(false)
+
+  const handleanswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+    setclicked(true)
+    
+    const nextvalue = currentvalue + 1
+    if (nextvalue < questions.length) {
+      setTimeout(() => {
+        setCurrentvalue(nextvalue)
+        setclicked(false)
+      }, 1000)
+      
+    }
+    else {
+      setShowScore(true)
+    }
+
+  }
+  // const background = (isCorrect) =>{
+  //   if (isCorrect) {
+  //     setansbg(Right)
+  //   }else{
+  //     setansbg(wrong)
+
+  //   }
+
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore
+        ?
+        (<div className='maincontainer'>
+          <h1 style={{ fontSize: "2.5rem", fontStyle: "italic" }}>🎉 Congratulation 🎉</h1>
+          <br />
+          <h1>You scored {score} out of {questions.length}</h1>
+        </div>)
+        : (<>
+          <div className='maincontainer'>
+
+            <div className='header'>
+              <h1>JS & React Quiz</h1>
+              <h2>Current Score : {score}</h2>
+            </div>
+
+            <div className='question-section'>
+              <h2>Questions {currentvalue + 1} out of {questions.length}</h2>
+              <h3> {questions[currentvalue].questionText}</h3>
+              {/* {console.log(questions[currentvalue].questionText)} */}
+              <div className='answer-section'>
+
+                {questions[currentvalue].answerOptions.map((ele) => {
+                  return <button className={`button ${clicked && ele.isCorrect?"correct":"button"}`} onClick={() => {
+                    handleanswer(ele.isCorrect)
+                  
+                    
+                  }}>{ele.answerText} </button>
+
+
+                })}
+              </div>
+
+            </div>
+          </div>
+        </>)}
+
+
     </div>
   );
 }
---
+++ src/Components/Questions.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+        questionText: '1) Which function is used to serialize an object into a JSON string in Javascript?',
+        answerOptions: [
+            { answerText: 'stringify()', isCorrect: true },
+            { answerText: 'parse()', isCorrect: false },
+            { answerText: 'convert()', isCorrect: false },
+            { answerText: 'None of the above', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '2) Which of the following methods can be used to display data in some form using Javascript?',
+        answerOptions: [
+            { answerText: 'document.write()', isCorrect: false },
+            { answerText: 'console.log()', isCorrect: false },
+            { answerText: 'window.alert', isCorrect: false },
+            { answerText: 'All of the above', isCorrect: true },
+        ],
+    },
+    {
+        questionText: '3) In which language is React.js written?',
+        answerOptions: [
+            { answerText: 'python', isCorrect: false },
+            { answerText: 'JavaScrript', isCorrect: true },
+            { answerText: 'Java', isCorrect: false },
+            { answerText: 'PHP', isCorrect: false },
+        ],
+    },
+    {
+        questionText: '4) JSX stands for __________',
+        answerOptions: [
+            { answerText: 'JavaScript XML', isCorrect: true },
+            { answerText: 'JSON XML', isCorrect: false },
+            { answerText: 'JSON', isCorrect: false },
+            { answerText: 'JavaScript and Angular', isCorrect: false },
+        ],
+    },
+    {
+        questionText: ' 5) What is ReactJS?',
+        answerOptions: [
+            { answerText: 'Server-side framework', isCorrect: false },
+            { answerText: 'A JavaScript library for building user interfaces', isCorrect: true },
+            { answerText: 'both a and b', isCorrect: false },
+            { answerText: 'None of the above', isCorrect: false },
+        ],
+    },
+];
